ace8251e4b7e234571bd30835c4e7336483d561f
jackrobert0220 <jackrobert0220@gmail.com>
2022-07-21 16:52:19 -0700

    socail links finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+
+      
+      <SocialLinks />
     </div>
   );
 }
---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { FaGithub, FaLinkedin, FaSpotify } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+
+const SocialLinks = () => {
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/jack-glazer/",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/jackrobert0220/",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Spotify <FaSpotify size={30} />
+        </>
+      ),
+      href: "https://open.spotify.com/artist/7FXugc5Lme1dHnjxD88do4?si=n9eH1rx9QSaSJsyhacFLmQ",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Mail <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:jackrobert0220@gmail.com",
+      style: "rounded-br-md",
+    },
+  ];
+
+  return (
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        {links.map(({id, child, href, style}) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-r-2xl duration-200 bg-[#47B0AC]" +
+              " " +
+              style
+            }
+          >
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SocialLinks;
