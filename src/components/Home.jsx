80ba4130f407e6bb0396c332a67c02ab81fba456
jackrobert0220 <jackrobert0220@gmail.com>
2022-07-25 10:16:30 -0700

    fixed img cutoff issue ' git push

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -72,7 +72,8 @@ const Home = () => {
       className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div className="rounded-2xl mx-auto w-2/3 md:w-[10] mt-4 sm:mt-0 bg-gradient-to-l p-[6px] from-[#47B0AC]  to-black">
+      
+        <div className="rounded-2xl mx-auto sm:mx-6 w-2/3 md:w-[10] mt-[10rem] md:my-auto bg-gradient-to-l p-[6px] from-[#47B0AC]  to-black max-w-[330px]">
           <div className="">
             <img
               src={HeroImage}
@@ -81,11 +82,11 @@ const Home = () => {
             />
           </div>
         </div>
-        <div className="flex flex-col justify-center md:h-2/3">
-          <h2 className="text-4xl  pt-10 sm:text-7xl md:mt-0 font-bold text-white">
+        <div className="flex flex-col justify-center md:h-2/3 ml-5">
+          <h2 className="text-4xl  pt-10 sm:text-5xl md:mt-0 font-bold text-white">
             Hi, I'm <span className="text-[#47B0AC]">Jack</span>👋
           </h2>
-          <h3 className="text-2xl sm:text-4xl font-bold text-white">
+          <h3 className="text-2xl sm:text-3xl font-bold text-white">
             I'm a Front-End Developer.
           </h3>
           <ul className="text-white text-xl py-4">
