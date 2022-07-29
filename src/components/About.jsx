2eac3940c5ff0422dbd1961940ea1f38135029d9
jackrobert0220 <jackrobert0220@gmail.com>
2022-07-29 16:28:52 -0700

    fixed about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
         <p className="text-2xl pt-4">I'm always creating!</p>
         <p className="text-xl mt-10">
           I primarily use <strong>React JS</strong> to build most of my projects, but picking up a
-          new framework or language isn't a issue. Lately, I have been utilizing <strong>Tailwind</strong> learning about its awesome features. I am comfortable working with backend technologies as well.
+          new framework or language isn't a issue. Lately, I have been utilizing <strong>Tailwind</strong> and learning about its awesome features. I am comfortable working with backend technologies as well.
         </p>
 
         <br />
@@ -25,9 +25,7 @@ const About = () => {
           journey, I have worked several jobs in hospitality. For the last six
           years, I worked in the funeral industry as a Funeral Director and
           Crematory Operator. It was the perfect opportunity to help people
-          through a difficult time. When I started to assist with submitting
-          order forms, I became curious about the inner workings of the
-          applications used.
+          through a difficult time. Soon, I became curious about the inner workings of the web-based applications used on the job.
         </p>
 
         <br />
@@ -36,8 +34,8 @@ const About = () => {
           In 2021, I began taking online courses and attended a bootcamp to deep
           dive into the world of programming. Ever since my first project, I
           have been hooked on learning as much as I can. Just like with making
-          music, it is beyond rewarding to make something and send it off into
-          the world and have it impact the lives of others. Currently, I spend
+          music, it is so rewarding to make something and send it off into
+          the world and have an impact the lives of others. Currently, I spend
           my time building with React JS and learning new technologies.
         </p>
       </div>
